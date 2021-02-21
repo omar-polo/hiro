26b7ec0a027886ed74b7ab61dcb7c5b852cca99c
Omar Polo <op@omarpolo.com>
2021-02-21 15:34:34 +0000

    fix compat layer for strtonum

--- util.c
+++ util.c
@@ -18,6 +18,8 @@
 
 #include "err.h"
 
+#include "strtonum.h"
+
 #include <limits.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -33,58 +35,6 @@ default_socket_path(void)
 
 	return "/tmp/hirod.1000";
 }
-
-/* XXX: fix this with a compat layer */
-
-#include <errno.h>
-#include <limits.h>
-#include <stdlib.h>
-
-#define	INVALID		1
-#define	TOOSMALL	2
-#define	TOOLARGE	3
-
-static long long
-strtonum(const char *numstr, long long minval, long long maxval,
-    const char **errstrp)
-{
-	long long ll = 0;
-	int error = 0;
-	char *ep;
-	struct errval {
-		const char *errstr;
-		int err;
-	} ev[4] = {
-		{ NULL,		0 },
-		{ "invalid",	EINVAL },
-		{ "too small",	ERANGE },
-		{ "too large",	ERANGE },
-	};
-
-	ev[0].err = errno;
-	errno = 0;
-	if (minval > maxval) {
-		error = INVALID;
-	} else {
-		ll = strtoll(numstr, &ep, 10);
-		if (numstr == ep || *ep != '\0')
-			error = INVALID;
-		else if ((ll == LLONG_MIN && errno == ERANGE) || ll < minval)
-			error = TOOSMALL;
-		else if ((ll == LLONG_MAX && errno == ERANGE) || ll > maxval)
-			error = TOOLARGE;
-	}
-	if (errstrp != NULL)
-		*errstrp = ev[error].errstr;
-	errno = ev[error].err;
-	if (error)
-		ll = 0;
-
-	return (ll);
-}
-
-
-/* XXX: end of XXX */
 
 int
 parse_portno(const char *p)
